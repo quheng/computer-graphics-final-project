56f24f896141dfcf0b3a9430dd8f32ffb23da33f
quheng <quheng@quhengdeMacBook-Pro.local>
2015-06-12 11:19:28 +0800

    add obj

---
+++ main.cpp
@@ -0,0 +1,226 @@
+#include <stdlib.h>
+#include <vector>
+#include <gl/GLUT.h>
+#include "glm/glm.hpp"
+#include "obj.h"
+
+using namespace std;
+float fTranslate;
+float fRotate;
+float fScale = 1.0f;	// set inital scale value to 1.0f
+float test = 0;
+
+bool bPersp = false;
+bool bAnim = false;
+bool bWire = false;
+
+float eye[] = { 0, 0, 8 };
+float center[] = { 0, 0, 0 };
+
+int wHeight = 0;
+int wWidth = 0;
+
+//obj
+vector<glm::vec3> out_vertices;
+vector<glm::vec2> out_uvs;
+vector<glm::vec3> out_normals;
+
+void Draw_Leg();
+
+void Draw_Triangle() // This function draws a triangle with RGB colors
+{
+	glPushMatrix();
+	glTranslatef(0, 0, 4 + 1);
+	glRotatef(90, 1, 0, 0);
+	glutSolidTeapot(1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0, 0, 3.5);
+	glScalef(5, 4, 1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(1.5, 1, 1.5);
+	Draw_Leg();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-1.5, 1, 1.5);
+	Draw_Leg();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(1.5, -1, 1.5);
+	Draw_Leg();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-1.5, -1, 1.5);
+	Draw_Leg();
+	glPopMatrix();
+
+}
+
+void Draw_Leg()
+{
+	glScalef(1, 1, 3);
+	glutSolidCube(1.0);
+}
+
+void updateView(int width, int height)
+{
+	glViewport(0, 0, width, height);						// Reset The Current Viewport
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+
+	float whRatio = (GLfloat)width / (GLfloat)height;
+	if (bPersp) {
+		gluPerspective(45.0f, whRatio, 0.1f, 100.0f);
+		//glFrustum(-3, 3, -3, 3, 3,100);
+	}
+	else {
+		glOrtho(-3, 3, -3, 3, -100, 100);
+	}
+
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+}
+
+void reshape(int width, int height)
+{
+	if (height == 0)										// Prevent A Divide By Zero By
+	{
+		height = 1;										// Making Height Equal One
+	}
+
+	wHeight = height;
+	wWidth = width;
+
+	updateView(wHeight, wWidth);
+}
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+void key(unsigned char k, int x, int y)
+{
+	switch (k)
+	{
+	case 27:
+	case 'q': {exit(0); break; }
+	case 'p': {bPersp = !bPersp; break; }
+
+	case ' ': {bAnim = !bAnim; break; }
+	case 'o': {bWire = !bWire; break; }
+
+	case 'a': {
+		eye[0] -= 0.2f;
+		center[0] -= 0.2f;
+		break;
+	}
+	case 'd': {
+		eye[0] += 0.2f;
+		center[0] += 0.2f;
+		break;
+	}
+	case 'w': {
+		eye[1] -= 0.2f;
+		center[1] -= 0.2f;
+		break;
+	}
+	case 's': {
+		eye[1] += 0.2f;
+		center[1] += 0.2f;
+		break;
+	}
+	case 'z': {
+		eye[2] -= 0.2f;
+		center[2] -= 0.2f;
+		break;
+	}
+	case 'c': {
+		eye[2] += 0.2f;
+		center[2] += 0.2f;
+		break;
+	}
+	case 'Z': {
+		test += 0.1;
+		break;
+	}
+	case 'C': {
+		eye[2] += 0.2f;
+		center[2] += 0.2f;
+		break;
+	}
+	}
+
+	updateView(wHeight, wWidth);
+}
+
+
+void redraw()
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();									// Reset The Current Modelview Matrix
+
+	gluLookAt(eye[0], eye[1], eye[2],
+		center[0], center[1], center[2],
+		0, 1, 0);
+
+	if (bWire) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	else {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_pos[] = { 5, 5, 5, 1 };
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, white);
+	glEnable(GL_LIGHT0);
+
+	//	glTranslatef(0.0f, 0.0f,-6.0f);			// Place the triangle at Center
+	glRotatef(fRotate, 0, 1.0f, 0);			// Rotate around Y axis
+	glRotatef(-90, 1, 0, 0);
+	glScalef(0.2, 0.2, 0.2);
+	//Draw_Triangle();						// Draw triangle
+	drawOBJ(out_vertices, out_normals);
+	if (bAnim) fRotate += 0.5f;
+	glutSwapBuffers();
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	drawOBJ(out_vertices, out_normals);
+	glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+	loadOBJ("boy.obj", out_vertices, out_uvs, out_normals);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowPosition(10, 10);
+	glutInitWindowSize(480, 480);
+	int windowHandle = glutCreateWindow("Simple GLUT App");
+
+	glutDisplayFunc(redraw);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(key);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+	return 0;
+}
+
+
